8f0a789577b47bed619e80af659876a587dd5178
zubogain <zubogain@gmail.com>
2018-08-11 21:38:27 +0600

    Refactoring webpack.config and added babel traspiler stage-3

--- webpack.config.js
+++ webpack.config.js
@@ -5,22 +5,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const { VueLoaderPlugin } = require('vue-loader');
+const JavaScriptObfuscator = require('webpack-obfuscator');
 
-module.exports = {
+const config = {
     entry: './src/js/app.js',
-    devtool: 'inline-source-map',
-
     output: {
         path: path.resolve(__dirname, 'dist'),
         publicPath: '/',
-        filename: 'bundle.js'
+        filename: '[name].js'
     },
     devServer: {
         historyApiFallback:{
             index:'/index.html'
         },
     },
-
     plugins: [
         new webpack.DefinePlugin({ "process.env.NODE_ENV": JSON.stringify("production") }),
         new MiniCssExtractPlugin({
@@ -28,14 +26,12 @@ module.exports = {
             chunkFilename: "[id].css"
         }),
         new HtmlWebpackPlugin({
-            title: 'Custom template',
+            title: 'Twitter-Project',
             template: "./src/template.html"
         }),
         new CleanWebpackPlugin(['dist']),
         new VueLoaderPlugin(),
-        new UglifyJsPlugin()
     ],
-
     module: {
         rules: [
             {
@@ -47,23 +43,91 @@ module.exports = {
             },
             {
                 test: /\.vue$/,
+                exclude: /node_modules/,
                 loader: 'vue-loader'
+            },
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            bypassOnDebug: true,
+                            disable: true,
+                        },
+                    },
+                ],
             }
         ]
     },
-
     optimization: {
-        minimizer: [
+        splitChunks: {
+            chunks: "initial"
+        }
+    }
+};
+
+module.exports = (env, argv) => {
+    if(argv.mode === 'production') {
+        config.mode = "production";
+        config.plugins.push(
+            new JavaScriptObfuscator ({
+                compact: true,
+                controlFlowFlattening: false,
+                controlFlowFlatteningThreshold: 0.75,
+                deadCodeInjection: false,
+                deadCodeInjectionThreshold: 0.4,
+                debugProtection: false,
+                debugProtectionInterval: false,
+                disableConsoleOutput: false,
+                domainLock: [],
+                identifierNamesGenerator: 'hexadecimal',
+                identifiersPrefix: '',
+                inputFileName: '',
+                log: false,
+                renameGlobals: false,
+                reservedNames: [],
+                reservedStrings: [],
+                rotateStringArray: true,
+                seed: 2048,
+                selfDefending: false,
+                sourceMap: false,
+                sourceMapBaseUrl: '',
+                sourceMapFileName: '',
+                sourceMapMode: 'separate',
+                stringArray: true,
+                stringArrayEncoding: false,
+                stringArrayThreshold: 0.75,
+                target: 'browser',
+                transformObjectKeys: false,
+                unicodeEscapeSequence: false
+            }, "vendors~main.js"),
+            new UglifyJsPlugin(),
+        );
+        config.optimization.minimizer = [
             new UglifyJsPlugin({
                 cache: true,
                 parallel: true,
                 uglifyOptions: {
                     compress: false,
-                    ecma: 6,
+                    ecma: 5,
                     mangle: true
                 },
                 sourceMap: false
             })
-        ]
+        ];
+        config.module.rules.push(
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        );
+    } else {
+        config.mode = "development";
     }
+    return config;
 };
